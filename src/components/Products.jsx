15d58f97cb62a9ee52b5e2c9204043131646a5f6
Sandeep Bhatula <sandeepbvv11@users.noreply.github.com>
2022-08-27 11:42:22 -0700

    Retrieval of the images.
    Display of products retrieved from the url.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -30,100 +30,21 @@ const Products = () => {
                 {/* <!--/.sample products just to show the page layout. Should retrieve the products from the url to display--> */}
                 <div className="products-content">
                     <div className="row">
-                       {data.forEach(element => {
-                        <p>{element.fulhausProductName}</p>
-                        })}
+                    {data.map(e =>(
                         <div className="col-md-3 col-sm-4">
                             <div className="single-new-arrival">
                                 <div className="single-new-arrival-bg">
-                                    <img src="assets/images/collection/arrivals1.png" alt="products images" />
+                                    <img src={e.imageURLs[0]} alt="products images" />
                                     <div className="single-new-arrival-bg-overlay"></div>
                                     
                                     
                                 </div>
-                                <h4><a href="#">wooden chair</a></h4>
-                                <p className="arrival-product-price">$65.00</p>
+                                <h4><a href="#">{e.fulhausProductName}</a></h4>
+                                <p className="arrival-product-price">{e.retailPrice}</p>
                             </div>
                         </div>
-                        <div className="col-md-3 col-sm-4">
-                            <div className="single-new-arrival">
-                                <div className="single-new-arrival-bg">
-                                    <img src="assets/images/collection/arrivals2.png" alt="products images" />
-                                    <div className="single-new-arrival-bg-overlay"></div>
-                                                                        
-                                </div>
-                                <h4><a href="#">single armchair</a></h4>
-                                <p className="arrival-product-price">$80.00</p>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-4">
-                            <div className="single-new-arrival">
-                                <div className="single-new-arrival-bg">
-                                    <img src="assets/images/collection/arrivals3.png" alt="products images" />
-                                    <div className="single-new-arrival-bg-overlay"></div>
-                                    
-                                </div>
-                                <h4><a href="#">wooden armchair</a></h4>
-                                <p className="arrival-product-price">$40.00</p>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-4">
-                            <div className="single-new-arrival">
-                                <div className="single-new-arrival-bg">
-                                    <img src="assets/images/collection/arrivals4.png" alt="products images" />
-                                    <div className="single-new-arrival-bg-overlay"></div>
-                                                                        
-                                </div>
-                                <h4><a href="#">stylish chair</a></h4>
-                                <p className="arrival-product-price">$100.00</p>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-4">
-                            <div className="single-new-arrival">
-                                <div className="single-new-arrival-bg">
-                                    <img src="assets/images/collection/arrivals5.png" alt="products images" />
-                                    <div className="single-new-arrival-bg-overlay"></div>
-                                    
-                                </div>
-                                <h4><a href="#">modern chair</a></h4>
-                                <p className="arrival-product-price">$120.00</p>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-4">
-                            <div className="single-new-arrival">
-                                <div className="single-new-arrival-bg">
-                                    <img src="assets/images/collection/arrivals6.png" alt="products images" />
-                                    <div className="single-new-arrival-bg-overlay"></div>
-                                    
-                                    
-                                </div>
-                                <h4><a href="#">mapple wood dinning table</a></h4>
-                                <p className="arrival-product-price">$140.00</p>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-4">
-                            <div className="single-new-arrival">
-                                <div className="single-new-arrival-bg">
-                                    <img src="assets/images/collection/arrivals7.png" alt="products images" />
-                                    <div className="single-new-arrival-bg-overlay"></div>
-                                    
-                                    
-                                </div>
-                                <h4><a href="#">arm chair</a></h4>
-                                <p className="arrival-product-price">$90.00</p>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-4">
-                            <div className="single-new-arrival">
-                                <div className="single-new-arrival-bg">
-                                    <img src="assets/images/collection/arrivals8.png" alt="new-arrivals images" />
-                                    <div className="single-new-arrival-bg-overlay"></div>
-                                    
-                                </div>
-                                <h4><a href="#">wooden bed</a></h4>
-                                <p className="arrival-product-price">$140.00</p>
-                            </div>
-                        </div>
+                    ))}    
+                        
                     </div>
                 </div>
             </div>
